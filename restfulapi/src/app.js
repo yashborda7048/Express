3f825173a6cc442f0cb787508c197b92f02ce776
yashborda7048 <yashborda123@gmail.com>
2023-10-19 20:10:43 +0530

    add  async and awit method for post api

--- restfulapi/src/app.js
+++ restfulapi/src/app.js
@@ -1,30 +1,33 @@
 const express = require("express");
 require("./db/conn");
-const Student = require("./db/models/students")
+const Student = require("./db/models/students");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// create a new students 
-app.get("/students", (req, res) => {
-  const user = new Student().find()
-  user.save().then(() => {
-      res.status(201).send(user);
-  }).catch((e) => {
-    res.status(400).send(e);
-  })
-});
+// // First Method
+// app.post("/students", (req, res) => {
+//   console.log(req.body);
+//   const user = new Student(req.body)
+//   user.save().then(() => {
+//       res.status(201).send(user);
+//   }).catch((e) => {
+//     res.status(400).send(e);
+//   })
+// });
 
-app.post("/students", (req, res) => {
-  console.log(req.body);
-  const user = new Student(req.body)
-  user.save().then(() => {
-      res.status(201).send(user);
-  }).catch((e) => {
+// // async and awit use
+app.post("/students", async (req, res) => {
+  try {
+    const user = new Student(req.body);
+    const createUser = await user.save();
+    res.status(201).send(createUser);
+  } catch (e) {
+    console.log(e);
     res.status(400).send(e);
-  })
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
