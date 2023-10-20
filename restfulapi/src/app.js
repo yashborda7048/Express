68c3e62cdc2c6bbf0758834b86acd9d408c0504d
yashborda7048 <yashborda123@gmail.com>
2023-10-20 16:53:50 +0530

    update and delete API

--- restfulapi/src/app.js
+++ restfulapi/src/app.js
@@ -21,7 +21,7 @@ app.get("/students", async (req, res) => {
 app.get("/students/:id", async (req, res) => {
   try {
     const _id = req.params.id;
-    const studentData = await Student.findById({ _id});
+    const studentData = await Student.findById({ _id });
     if (!studentData) {
       return res.status(404).send("Not valid Id");
     } else {
@@ -40,6 +40,38 @@ app.post("/students", async (req, res) => {
     res.status(201).send(createUser);
   } catch (e) {
     console.log(e, "error");
+    res.status(400).send(e);
+  }
+});
+
+// update the indivisual Students data using ID
+app.patch("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const updateStudent = await Student.findByIdAndUpdate(_id, req.body, {
+      new: true,
+    });
+    if (!updateStudent) {
+      return res.status(404).send("Not valid Id");
+    } else {
+      res.status(201).send(updateStudent);
+    }
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// delete the indivisual Students data using ID
+app.delete("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const deleteStudent = await Student.findByIdAndDelete({ _id });
+    if (!deleteStudent) {
+      return res.status(404).send("Not valid Id");
+    } else {
+      res.status(201).send(deleteStudent);
+    }
+  } catch (e) {
     res.status(400).send(e);
   }
 });
