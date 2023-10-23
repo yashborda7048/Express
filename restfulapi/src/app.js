1497fc0f3624b7560475cbc604b3a0ff76f64527
yashborda7048 <yashborda123@gmail.com>
2023-10-23 17:09:29 +0530

    add router

--- restfulapi/src/app.js
+++ restfulapi/src/app.js
@@ -1,79 +1,10 @@
 const express = require("express");
 require("./db/conn");
-const Student = require("./db/models/students");
+const studentRouter = require('./routers/student')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-// get the data of registered Students
-app.get("/students", async (req, res) => {
-  try {
-    const studentData = await Student.find();
-    res.status(201).send(studentData);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-// get the indivisual Students data using ID
-app.get("/students/:id", async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const studentData = await Student.findById({ _id });
-    if (!studentData) {
-      return res.status(404).send("Not valid Id");
-    } else {
-      res.status(201).send(studentData);
-    }
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-// create a new students
-app.post("/students", async (req, res) => {
-  try {
-    const user = new Student(req.body);
-    const createUser = await user.save();
-    res.status(201).send(createUser);
-  } catch (e) {
-    console.log(e, "error");
-    res.status(400).send(e);
-  }
-});
-
-// update the indivisual Students data using ID
-app.patch("/students/:id", async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const updateStudent = await Student.findByIdAndUpdate(_id, req.body, {
-      new: true,
-    });
-    if (!updateStudent) {
-      return res.status(404).send("Not valid Id");
-    } else {
-      res.status(201).send(updateStudent);
-    }
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-// delete the indivisual Students data using ID
-app.delete("/students/:id", async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const deleteStudent = await Student.findByIdAndDelete({ _id });
-    if (!deleteStudent) {
-      return res.status(404).send("Not valid Id");
-    } else {
-      res.status(201).send(deleteStudent);
-    }
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
+app.use(studentRouter);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ restfulapi/src/routers/student.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const router = new express.Router();
+const Student = require("../models/students");
+
+// get the data of registered Students
+router.get("/students", async (req, res) => {
+  try {
+    const studentData = await Student.find();
+    res.status(201).send(studentData);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// get the indivisual Students data using ID
+router.get("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const studentData = await Student.findById({ _id });
+    if (!studentData) {
+      return res.status(404).send("Not valid Id");
+    } else {
+      res.status(201).send(studentData);
+    }
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// create a new students
+router.post("/students", async (req, res) => {
+  try {
+    const user = new Student(req.body);
+    const createUser = await user.save();
+    res.status(201).send(createUser);
+  } catch (e) {
+    console.log(e, "error");
+    res.status(400).send(e);
+  }
+});
+
+// update the indivisual Students data using ID
+router.patch("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const updateStudent = await Student.findByIdAndUpdate(_id, req.body, {
+      new: true,
+    });
+    if (!updateStudent) {
+      return res.status(404).send("Not valid Id");
+    } else {
+      res.status(201).send(updateStudent);
+    }
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// delete the indivisual Students data using ID
+router.delete("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const deleteStudent = await Student.findByIdAndDelete({ _id });
+    if (!deleteStudent) {
+      return res.status(404).send("Not valid Id");
+    } else {
+      res.status(201).send(deleteStudent);
+    }
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+module.exports = router;
